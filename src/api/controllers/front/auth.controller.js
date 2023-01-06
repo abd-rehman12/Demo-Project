77a63c18ff40eafb88ff47885eb863657ba67962
owaisali01 <owais.ali9091@gmail.com>
2023-01-06 17:12:29 +0500

    Main gfdd

---
+++ src/api/controllers/front/auth.controller.js
@@ -0,0 +1,4 @@
+
+exports.register = async (req, res, next) => {
+  res.send("Regestired Successfully!")
+}
---
+++ src/api/routes/v1/admin/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const settingsRoutes = require('./settings.route')
+const contactRoutes = require('./contact.route')
+const flightroutes=require('./flights.route')
+const ticketroutes=require('./tickets.route')
+const passengerRoutes=require('./passenger.route.js')
+const hotelListRoutes= require('./hotelList.route')
+
+const router = express.Router()
+
+/**
+ * GET v1/admin
+ */
+
+router.use('/settings', settingsRoutes)
+router.use('/contacts', contactRoutes)
+router.use('/flights',flightroutes);
+router.use('/tickets',ticketroutes)
+router.use('/passenger',passengerRoutes)
+router.use('/hotelList', hotelListRoutes)
+module.exports = router
